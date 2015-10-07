782af913134191b47b7b9aabad01ffd9bac0f341
EvanLu <cllu.tw@gmail.com>
2015-10-07 08:39:31 +0800

    update comment

--- ELSimpleAlertView/ELSimpleAlertView.h
+++ ELSimpleAlertView/ELSimpleAlertView.h
@@ -12,6 +12,17 @@ typedef void (^ actionBlock)(id);
 
 @interface ELSimpleAlertView : UIViewController <UIAlertViewDelegate>
 
+/**
+ *  show default alert view with cancel/confirm buttons
+ *
+ *  @param promptMessage:      messages that you'd like to display
+ *  @param title:              title of alert view
+ *  @param cancelButtonTitle:  title of cancel button
+ *  @param confirmButtonTitle: title of confirm button
+ *  @param cancelButtonBlock:  pass a block to function if you need to do something after cancel button pressed, could be nil.
+ *  @param confirmButtonBlock: pass a block to function if you need to do something after confirm button pressed, could be nil.
+ *  @param blockArgument       pass the parameter that used in callback function, could be nil.
+ */
 -(void)showAlertViewWithDefaultStyle:(NSString *)promptMessage
                                title:(NSString *)title
                    cancelButtonTitle:(NSString *)cancelButtonTitle
@@ -20,6 +31,16 @@ typedef void (^ actionBlock)(id);
          confirmButtonActionCallback:(actionBlock)confirmButtonBlock
                        blockArgument:(id)blockArgument;
 
+
+/**
+ *  show default alert view with only cancel button
+ *
+ *  @param promptMessage:      messages that you want to display
+ *  @param title:              title of alert view
+ *  @param confirmButtonTitle: title of confirm button
+ *  @param confirmButtonBlock: pass a block to function if you need to do something after cancel button pressed, could be nil.
+ *  @param argument            pass the parameter that used in callback function, could be nil.
+ */
 -(void)showAlertViewDefaultStyleOnlyCancelButton:(NSString *)promptMessage
                                            title:(NSString *)title
                                cancelButtonTitle:(NSString *)cancelButtonTitle
